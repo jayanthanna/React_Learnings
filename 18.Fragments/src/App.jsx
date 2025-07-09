67b5c37341996b36460e1083e5dbdc29608a2107
Jayanth <annajayanth27@gmail.com>
2025-07-09 21:50:20 +0530

    Using Map-method & importance of key prop

--- 18.Fragments/src/App.jsx
+++ 18.Fragments/src/App.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
+  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk"];
+
   return (
     <React.Fragment>
-    {/* <> */}
+      {/* <> */}
       <h1>Healthy Food</h1>
-      <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
+      <ul className="list-group">
+        {/* <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Green Vegetable</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Salad</li>
+        <li class="list-group-item">Milk</li> */}
+        {foodItems.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
-    {/* </> */}
+      {/* </> */}
     </React.Fragment>
   );
 };
