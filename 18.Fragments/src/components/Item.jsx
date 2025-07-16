b377c75e22c10855323fbcca3f21547be54dfc1f
Jayanth <annajayanth27@gmail.com>
2025-07-16 21:44:59 +0530

    Using css modules

--- 18.Fragments/src/components/Item.jsx
+++ 18.Fragments/src/components/Item.jsx
@@ -1,8 +1,15 @@
+import styles from "./Item.module.css";
+
 // const Item = (props) => {
 const Item = ({ foodItem }) => {
   // let { foodItem } = props;
+  // return (
+  //   <li className="list-group-item kg-item">
+  //     <span>{foodItem}</span>
+  //   </li>
+  // );
   return (
-    <li className="list-group-item kg-item">
+    <li className={`${styles["kg-item"]}`}>
       <span>{foodItem}</span>
     </li>
   );
